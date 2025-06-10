f123ea022af63f781fff6debe9515548097ca6f1
Roman Kalinkin <r.kalinkin@vk.team>
2025-06-10 13:59:54 +0300

    hw03-annotations

---
+++ hw03-annotations/src/main/java/ru/otus/Runner.java
@@ -0,0 +1,92 @@
+package ru.otus;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.annotations.After;
+import ru.otus.annotations.Before;
+import ru.otus.annotations.Test;
+
+public class Runner {
+
+    private static final Logger logger = LoggerFactory.getLogger(Runner.class);
+
+    private int totalTests = 0;
+    private int passedTests = 0;
+    private int failedTests = 0;
+
+    public static void run(Class<?> testClass) {
+        new Runner().runTests(testClass);
+    }
+
+    private void runTests(Class<?> testClass) {
+        Method[] methods = testClass.getDeclaredMethods();
+
+        List<Method> beforeMethods = getAnnotatedMethods(methods, Before.class);
+        List<Method> afterMethods = getAnnotatedMethods(methods, After.class);
+        List<Method> testMethods = getAnnotatedMethods(methods, Test.class);
+
+        for (Method testMethod : testMethods) {
+            runTest(testClass, beforeMethods, testMethod, afterMethods);
+        }
+
+        printSummary();
+    }
+
+    private List<Method> getAnnotatedMethods(
+            Method[] methods, Class<? extends java.lang.annotation.Annotation> annotation) {
+        List<Method> result = new ArrayList<>();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(annotation)) {
+                result.add(method);
+            }
+        }
+        return result;
+    }
+
+    private void runTest(Class<?> testClass, List<Method> beforeMethods, Method testMethod, List<Method> afterMethods) {
+        try {
+            Object instance = testClass.getDeclaredConstructor().newInstance();
+
+            invokeAll(instance, beforeMethods);
+            invokeTestMethod(instance, testMethod);
+            invokeAll(instance, afterMethods);
+
+            passedTests++;
+        } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause();
+            logger.error("Test failed: {} - {}", testMethod.getName(), cause.getMessage(), cause);
+            failedTests++;
+        } catch (Exception e) {
+            logger.error("Error during test execution: {}", testMethod.getName(), e);
+            failedTests++;
+        } finally {
+            totalTests++;
+        }
+    }
+
+    private void invokeAll(Object instance, List<Method> methods) throws Exception {
+        for (Method method : methods) {
+            method.invoke(instance);
+        }
+    }
+
+    private void invokeTestMethod(Object instance, Method testMethod) throws Exception {
+        testMethod.invoke(instance);
+    }
+
+    private void printSummary() {
+        logger.info(
+                """
+                \n--- Test Summary ---
+                Total tests: {}
+                Passed: {}
+                Failed: {}""",
+                totalTests,
+                passedTests,
+                failedTests);
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,8 @@
 rootProject.name = "otus-training"
+
 include("hw01-gradle")
 include("hw02-generics")
+include("hw03-annotations")
 
 pluginManagement {
     val jgitver: String by settings
