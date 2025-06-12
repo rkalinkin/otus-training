0dfbc6f62b19755881ccea53265f9bb2037a66ec
Roman Kalinkin <57835341+rkalinkin@users.noreply.github.com>
2025-06-12 14:41:07 +0300

    hw04-gc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,8 @@ rootProject.name = "otus-training"
 include("hw01-gradle")
 include("hw02-generics")
 include("hw03-annotations")
+include("hw04-gc")
+
 
 pluginManagement {
     val jgitver: String by settings
