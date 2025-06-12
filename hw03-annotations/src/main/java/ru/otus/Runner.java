1ff9733f6125a3dbd788dd6032aeb578db39f8f3
Roman Kalinkin <57835341+rkalinkin@users.noreply.github.com>
2025-06-12 14:37:33 +0300

    hw03-annotations

---
+++ hw03-annotations/src/main/java/ru/otus/Runner.java
@@ -0,0 +1,99 @@
+package ru.otus;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.annotations.After;
+import ru.otus.annotations.Before;
+import ru.otus.annotations.Test;
+
+public class Runner {
+
+    private static final Logger logger = LoggerFactory.getLogger(Runner.class);
+
+    private int passedTests = 0;
+
+    public static void run(Class<?> testClass) {
+        new Runner().runTests(testClass);
+    }
+
+    private void runTests(Class<?> testClass) {
+        Method[] methods = testClass.getDeclaredMethods();
+
+        List<Method> beforeMethods = getAnnotatedMethods(methods, Before.class);
+        List<Method> afterMethods = getAnnotatedMethods(methods, After.class);
+        List<Method> testMethods = getAnnotatedMethods(methods, Test.class);
+
+        for (Method testMethod : testMethods) {
+            runTest(testClass, beforeMethods, testMethod, afterMethods);
+        }
+
+        printSummary(testMethods.size(), passedTests);
+    }
+
+    private List<Method> getAnnotatedMethods(
+            Method[] methods, Class<? extends java.lang.annotation.Annotation> annotation) {
+        List<Method> result = new ArrayList<>();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(annotation)) {
+                result.add(method);
+            }
+        }
+        return result;
+    }
+
+    private void runTest(Class<?> testClass, List<Method> beforeMethods, Method testMethod, List<Method> afterMethods) {
+        try {
+            Object instance = testClass.getDeclaredConstructor().newInstance();
+
+            try {
+                invokeAll(instance, beforeMethods);
+
+                try {
+                    invokeTestMethod(instance, testMethod);
+                    passedTests++;
+                } catch (InvocationTargetException e) {
+                    Throwable cause = e.getCause();
+                    logger.error("Test failed: {} - {}", testMethod.getName(), cause.getMessage(), cause);
+                } catch (Exception e) {
+                    logger.error("Error during test execution: {}", testMethod.getName(), e);
+                }
+
+            } finally {
+                try {
+                    invokeAll(instance, afterMethods);
+                } catch (Exception e) {
+                    logger.error("Exception in @After methods of '{}'", testMethod.getName(), e);
+                }
+            }
+
+        } catch (Exception e) {
+            logger.error("Failed to create instance or unexpected error in '{}'", testMethod.getName(), e);
+        }
+    }
+
+    private void invokeAll(Object instance, List<Method> methods) throws Exception {
+        for (Method method : methods) {
+            method.invoke(instance);
+        }
+    }
+
+    private void invokeTestMethod(Object instance, Method testMethod) throws Exception {
+        testMethod.invoke(instance);
+    }
+
+    private void printSummary(int totalTests, int passedTests) {
+        logger.info(
+                """
+                \n--- Test Summary ---
+                Total tests: {}
+                Passed: {}
+                Failed: {}""",
+                totalTests,
+                passedTests,
+                totalTests - passedTests);
+    }
+}
